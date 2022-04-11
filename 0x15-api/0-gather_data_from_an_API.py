d5e4bece54f605046e42d478934767598a6605e5
GITBOAT <boat232001@yahoo.com>
2022-04-11 07:30:43 -0700

    json

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import urllib.request
+import urllib.parse
+import json
+import sys
+""" Gather data from an API """
+
+
+def emp(id):
+    usr = 'https://jsonplaceholder.typicode.com/users'
+    url1 = 'https://jsonplaceholder.typicode.com/todos'
+    request_url = urllib.request.urlopen(usr)
+    data = request_url.read()
+    request_url1 = urllib.request.urlopen(url1)
+    todos1 = request_url1.read()
+    todos = json.loads(todos1)
+    jdata = json.loads(data)
+    emp_name = str
+    com = 0
+    tks = 0
+    emp_task = []
+
+    for i in jdata:
+        """ Search through for the id """
+        if i["id"] == id:
+            """ if i id found, check the number of task completd """
+            emp_name = i["name"]
+            for j in todos:
+                """ stor the title name of the  completed task """
+                if j["userId"] == id:
+                    if j["completed"]:
+                        emp_task.append(j["title"])
+                        com = com + 1
+                    tks = tks + 1
+
+    print("Employee {} is done with tasks ({}/{}):".format(emp_name, com, tks))
+    for i in emp_task:
+        print("\t", i)
+
+
+if __name__ == "__main__":
+    emp(int(sys.argv[1]))
