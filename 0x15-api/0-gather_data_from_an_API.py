52a58beb680ca8de92d892b00ec82135d5780460
GITBOAT <boat232001@yahoo.com>
2022-04-11 18:58:54 -0700

    json

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,12 +2,11 @@
 """ Gather data from an API """
 import json
 import sys
-import urllib.parse
 import urllib.request
 
 
 def emp(id):
-    usr = 'https://jsonplaceholder.typicode.com/users'
+    usr = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
     url1 = 'https://jsonplaceholder.typicode.com/todos'
     request_url = urllib.request.urlopen(usr)
     data = request_url.read()
@@ -15,29 +14,28 @@ def emp(id):
     todos1 = request_url1.read()
     todos = json.loads(todos1)
     jdata = json.loads(data)
-    emp_name = str
+    
     com = 0
     tks = 0
     emp_task = []
 
-    for i in jdata:
-        """ Search through for the id """
-        if i["id"] == id:
-            """ if i id found, check the number of task completd """
-            emp_name = i["name"]
-            for j in todos:
-                """ stor the title name of the  completed task """
-                if j["userId"] == id:
-                    if j["completed"]:
-                        emp_task.append(j["title"])
-                        com = com + 1
-                    tks = tks + 1
+
+    emp_name = jdata.get("name")
+    for j in todos:
+        """ stor the title name of the  completed task """
+        if j["userId"] == id:
+            if j["completed"]:
+                emp_task.append(j["title"])
+                com = com + 1
+            tks = tks + 1
 
     print("Employee {} is done with tasks({}/{}):".format(emp_name, com, tks))
     for i in emp_task:
         print("\t", end="")
         print(i)
+    print("\n\n...",emp_name,"....")
 
 
 if __name__ == "__main__":
     emp(int(sys.argv[1]))
+
